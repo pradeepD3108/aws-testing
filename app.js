2718d34ff814ea1e6f2c6445556c0e71285f8cd6
pradeepD3108 <122516697+pradeepD3108@users.noreply.github.com>
2023-08-10 15:57:01 +0530

    Create simple server

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const http = require('http');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const hostname = '0.0.0.0';  // Listen on all available network interfaces
+const port = 3000;           // Use any available port you prefer
 
-var app = express();
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
